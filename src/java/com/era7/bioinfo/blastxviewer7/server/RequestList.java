a9b1077040313aad5c6d5668468f61fc3978fc65
Pablo Pareja Tobes <ppareja@era7.com>
2011-04-26 13:03:40 +0200

    first commit

---
+++ src/java/com/era7/bioinfo/blastxviewer7/server/RequestList.java
@@ -0,0 +1,19 @@
+package com.era7.bioinfo.blastxviewer7.server;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+/**
+ *
+ * @author ppareja
+ */
+public class RequestList {
+
+    public static final String UPLOAD_BLAST_AND_GET_COVERAGE_XML_REQUEST = "upload_blast_and_get_coverage_xml";
+    public static final String LOAD_BLAST_FILE_FROM_URL_AND_GET_COVERAGE_XML_REQUEST = "load_blast_file_from_url_and_get_coverage_xml";
+
+}
